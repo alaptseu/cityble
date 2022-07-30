5c03d2ce8bd8de430d2b7a29471f03c3c79b9dbd
Laptseu <alexlaptseu1@Alexs-MacBook-Pro.local>
2022-07-30 21:35:10 +1000

    spelling mistake fixed

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,8 +10,8 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the city in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+        Guess the city name in 6 tries. After each guess, the color of the tiles will
+        change to show how close your guess was to the city name.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
