5ab39efd3acc39671671eea6292ea36e32a565b2
Laptseu <alexlaptseu1@Alexs-MacBook-Pro.local>
2022-08-05 08:45:53 +1000

    improved game descriptio

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -58,6 +58,11 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
       <p className="text-sm text-gray-500 dark:text-gray-300">
         The letter K is not in the city name in any spot.
       </p>
+      <div className="flex justify-center mb-1 mt-4">
+        <p className="text-sm text-gray-500 dark:text-gray-300">
+          Turn on <b>Show country name</b> in the settings to make it easier for you. Enjoy!
+        </p>
+      </div>
     </BaseModal>
   )
 }
